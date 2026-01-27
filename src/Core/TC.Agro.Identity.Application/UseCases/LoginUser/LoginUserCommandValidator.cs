5c759caf1e749b3de2241318d0f65a67b0684d5c
rdpresser <rodrigo.presser@gmail.com>
2026-01-27 01:05:35 -0300

    Simplify password validation in LoginUserCommandValidator
    Removed all password complexity requirements from LoginUserCommandValidator. Now, only non-empty passwords are required for login, eliminating checks for length, case, numbers, and special characters.

--- src/Core/TC.Agro.Identity.Application/UseCases/LoginUser/LoginUserCommandValidator.cs
+++ src/Core/TC.Agro.Identity.Application/UseCases/LoginUser/LoginUserCommandValidator.cs
@@ -1,4 +1,4 @@
-﻿namespace TC.Agro.Identity.Application.UseCases.LoginUser
+namespace TC.Agro.Identity.Application.UseCases.LoginUser
 {
     public sealed class LoginUserCommandValidator : Validator<LoginUserCommand>
     {
@@ -6,31 +6,16 @@
         {
             RuleFor(x => x.Email)
                 .NotEmpty()
-                .WithMessage("Email is required.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Email)}.Required")
+                    .WithMessage("Email is required.")
+                    .WithErrorCode($"{nameof(LoginUserCommand.Email)}.Required")
                 .EmailAddress()
-                .WithMessage("Invalid email format.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Email)}.InvalidEmailFormat");
+                    .WithMessage("Invalid email format.")
+                    .WithErrorCode($"{nameof(LoginUserCommand.Email)}.InvalidEmailFormat");
 
             RuleFor(x => x.Password)
                 .NotEmpty()
-                .WithMessage("Password is required.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Password)}.Required")
-                .MinimumLength(8)
-                .WithMessage("Password must be at least 8 characters long.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Password)}.MinimumLength")
-                .Matches(@"[A-Z]")
-                .WithMessage("Password must contain at least one uppercase letter.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Password)}.UppercaseLetter")
-                .Matches(@"[a-z]")
-                .WithMessage("Password must contain at least one lowercase letter.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Password)}.LowercaseLetter")
-                .Matches(@"\d")
-                .WithMessage("Password must contain at least one number.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Password)}.ContainNumber")
-                .Matches(@"[\W_]")
-                .WithMessage("Password must contain at least one special character.")
-                .WithErrorCode($"{nameof(LoginUserCommand.Password)}.SpecialCharacter");
+                    .WithMessage("Password is required.")
+                    .WithErrorCode($"{nameof(LoginUserCommand.Password)}.Required");
         }
     }
 }
