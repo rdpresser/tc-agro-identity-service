7da4e2f52ee389929061f0f5b179b1617913edf1
rdpresser <rodrigo.presser@gmail.com>
2026-03-05 12:46:11 -0300

    Add architecture tests to enforce layer boundaries
    Added TC.Agro.Identity.Architecture.Tests project with NetArchTest.Rules to verify no forbidden dependencies exist between Domain, Application, Infrastructure, and Presentation layers. Updated solution and package references accordingly.

---
+++ test/TC.Agro.Identity.Architecture.Tests/BaseTest.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using TC.Agro.Identity.Application.UseCases.CreateUser;
+using TC.Agro.Identity.Domain.Aggregates;
+using TC.Agro.Identity.Infrastructure;
+using TC.Agro.Identity.Service;
+
+namespace TC.Agro.Identity.Architecture.Tests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(UserAggregate).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(CreateUserCommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+}
